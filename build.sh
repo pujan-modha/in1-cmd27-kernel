cf8c78c6bac9eb8e26a32b6da575b2c2aa33788c
pujan <pujan@local>
2026-08-30 04:47:08 +0530

    fix: use crdroid clang mirror

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ git apply ../cmd27.patch
 grep -n "MMC_PROGRAM_CSD" drivers/mmc/host/mediatek/ComboA/sd.c
 cd ..
 
-git clone --depth=1 https://github.com/sarthakroy2002/clang-r353983c1 clang
+git clone --depth=1 https://github.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-5484270 clang
 git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 los-4.9-64
 git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 los-4.9-32
 
